6a6af481efb82dd227ab63e5f6d1c94e7513baef
Andrew <Deerhunter6393@aol.com>
2014-12-19 02:14:48 -0500

    commit core files

--- js/main.js
+++ js/main.js
@@ -1,15 +1,56 @@
 var albums;
 
+//template variables
+var artistLinkTemplate
+
 $(document).ready(function() {
-    $.getJSON( "data/music.json", function ( data ) {
+
+    //get references to DOM elements
+    var contentDiv = $("#content");
+    var artistDiv = $("#artist_page");
+    var infoDiv = $("#artistTab");
+
+
+    $.when (
+        $.ajax("components/components.html"),
+        $.getJSON( "data/music.json")
+    ).done(function( templateData, data) {
+
+        var templates = $(templateData[0]);
+
+        //compile templates
+        artistLinkTemplate = Handlebars.compile( templates.find("#artistLinks").html() );
 
         //store album data
         albums = data.albums;
 
-        for(var i = 0; i < data.albums.length; i++) {
-            $("#artist_music").append("<li data-index='"+i+"'>" + data.albums[i].artist + "</li>");
-        }
-
-
+        //append starting state
+        artistDiv.html( artistLinkTemplate( data[0].artist ));
     });
+
+        $("#content").on("click", ".artistLink", function() {
+            //text name of artist
+            var artistToFind = $(this).html();
+            //an object to hold out results
+            var results = {};
+
+            results.albums = _.where(albums, { artist: artistToFind});
+
+           infoDiv.html( artistLinkTemplate( results) );
+        });
+
+        //search field
+        $("#btnSearch").click(function() {
+            var searchTerm = $("#txtSearch").val();
+
+            var results = {};
+
+            //will only search exact titles
+            results.albums = _.filter(title, function(item) {
+                return (item.title.toUpperCase().indexOf(searchTerm.toUpperCase() != -1));
+            })
+
+            artistDiv.html( artistLinkTemplate( results));
+        })
+
 });
